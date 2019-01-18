defd67903510c731a866afd799fd5d7800a978f8
Mason Fidino <mfidino@lpzoo.org>
2019-01-18 08:54:19 -0600

    dropped beta function, not used

--- gallo_et_al_2019_AnimalEcology/2019-01-15_gallo_et_al_JAE_utility_script.R
+++ gallo_et_al_2019_AnimalEcology/2019-01-15_gallo_et_al_JAE_utility_script.R
@@ -19,18 +19,6 @@ df_2_array <- function(my_df = NULL){
   return(my_array)
 }
 
-
-betaABfromMeanSD = function( mean , sd ) {
-  if ( mean <=0 | mean >= 1) stop("must have 0 < mean < 1")
-  if ( sd <= 0 ) stop("sd must be > 0")
-  kappa = mean*(1-mean)/sd^2 - 1
-  if ( kappa <= 0 ) stop("invalid combination of mean and sd")
-  a = mean * kappa
-  b = ( 1.0 - mean ) * kappa
-  return( list( a=a , b=b ) )
-}
-
-
 overlap_mod <- function (A, B, xscale = 24, xcenter = c("noon", "midnight"), 
                          linetype = c(1,2), linecol = c("black", "blue"),
                          linewidth = c(1,1), olapcol = "lightgrey", rug = FALSE, 
