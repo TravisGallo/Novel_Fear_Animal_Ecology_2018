5bb3410a0194ede380ee031e69c74072354d029d
Mason Fidino <mfidino@lpzoo.org>
2019-01-18 08:52:58 -0600

    removed inits function from utility
    We have the correct inits function in the fit model script.

--- gallo_et_al_2019_AnimalEcology/2019-01-15_gallo_et_al_JAE_utility_script.R
+++ gallo_et_al_2019_AnimalEcology/2019-01-15_gallo_et_al_JAE_utility_script.R
@@ -30,49 +30,6 @@ betaABfromMeanSD = function( mean , sd ) {
   return( list( a=a , b=b ) )
 }
 
-inits <- function(chain){
-  gen_list <- function(chain = chain){
-    list( 
-      z = z,
-      psinit = rbeta(2, 1, 1),
-      pmu = rnorm(2, 0, 1),
-      lp = rnorm(2, 0, 1),
-      gmu = rnorm(2, 0, 1),
-      gyr = matrix(rnorm(10*2, 0, 1), ncol= 2, nrow = 10),
-      tau_yr = rgamma(2, 1, 1),
-      gb = matrix(rnorm(2), ncol = 2, nrow = 1),
-      pb = matrix(rnorm(2), ncol = 2, nrow = 1),
-      lpb = matrix(rnorm(2), ncol = 2, nrow = 1),
-      v0 = rnorm(1, 0, 1),
-      v1 = rnorm(1, 0, 1), 
-      vcoy0 = rnorm(1, 0, 1), 
-      vcoy1 = rnorm(1, 0, 1),
-      a = array(c(0,rnorm(1),0,rnorm(1),0,rnorm(1),0, rnorm(1)),dim=c(2,2,2)),
-      da = matrix(c(0,rnorm(1),0, rnorm(1)), ncol=2, nrow=2),
-      .RNG.name = switch(chain,
-                         "1" = "base::Wichmann-Hill",
-                         "2" = "base::Marsaglia-Multicarry",
-                         "3" = "base::Super-Duper",
-                         "4" = "base::Mersenne-Twister",
-                         "5" = "base::Wichmann-Hill",
-                         "6" = "base::Marsaglia-Multicarry",
-                         "7" = "base::Super-Duper",
-                         "8" = "base::Mersenne-Twister"),
-      .RNG.seed = sample(1:1e+06, 1)
-    )
-  }
-  return(switch(chain,           
-                "1" = gen_list(chain),
-                "2" = gen_list(chain),
-                "3" = gen_list(chain),
-                "4" = gen_list(chain),
-                "5" = gen_list(chain),
-                "6" = gen_list(chain),
-                "7" = gen_list(chain),
-                "8" = gen_list(chain)
-  )
-  )
-}
 
 overlap_mod <- function (A, B, xscale = 24, xcenter = c("noon", "midnight"), 
                          linetype = c(1,2), linecol = c("black", "blue"),
